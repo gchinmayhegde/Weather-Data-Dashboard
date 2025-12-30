3e448b4fa839985f4736b48d13d4b606b7fbe090
gchinmayhegde <chinmaygh9@gmail.com>
2025-12-30 15:48:28 +0530

    Valid Api Responses

--- whether-data-dashboard/src/App.js
+++ whether-data-dashboard/src/App.js
@@ -5,13 +5,39 @@ function App(){
 
   const [city, setCity] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   console.log(selectedCity);
   //console.log(city);
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
   useEffect(() => {
     console.log(selectedCity);
+    
+    if(selectedCity === "") return;
+
+    const fetchWeatherData = async ()=> {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`);
+        if (!response.ok) {
+          throw new Error("City not found");
+        }
+        const data = await response.json();
+        setWeatherData(data);
+        setLoading(false);
+      }
+       catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchWeatherData(); 
   }, [selectedCity]);
+
 
   return(
     <div className="App">
@@ -20,6 +46,19 @@ function App(){
 
       <input type="text" className="city-input" placeholder="Enter City Name" value={city} onChange={(e)=>setCity(e.target.value)}/>
       <button className="get-weather-btn" onClick={()=>setSelectedCity(city)}>Get Weather</button>
+
+      {loading && !error && <p>Loading weather data...</p>}
+      {error && <p style={{color:"red"}}>Error: {error}</p>}
+
+    {weatherData && !loading && !error && (
+      <div className="weather-data-main">
+        <h2>Weather in {weatherData.name}</h2>
+        <p>Temperature: {weatherData.main.temp} °C</p>
+        <p>Weather: {weatherData.weather[0].description}</p>
+        <p>Humidity: {weatherData.main.humidity} %</p>
+        <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+      </div>
+    )}
       
     </div>
   )
